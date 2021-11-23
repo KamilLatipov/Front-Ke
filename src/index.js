8ae956270a1a3ea6186e7f45d98c5b181a0026ca
Kamil <latipovkamil@mail.ru>
2021-11-23 10:38:35 +0300

    gh-pages

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './main.css';
-import './fonts.css';
-import './blocks/faq/dropdown';
-
-function importAll(r) {
-    r.keys().forEach(r);
-}
-
-importAll(require.context('./blocks', true, /\.css$/));
